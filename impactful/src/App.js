a66261ae2d321cd4d4d44bc00244042d6175a97b
Larissa <larissa.nguyen@yale.edu>
2020-05-16 14:39:40 -0400

    updating everything

--- impactful/src/App.js
+++ impactful/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import OurNavBar from './OurNavBar'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <OurNavBar/>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
