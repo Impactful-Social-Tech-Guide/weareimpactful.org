665311e9a13cd71fabe62d342fdfeca026647915
nsubbaian <nithmails1@gmail.com>
2020-06-06 00:59:56 -0400

    add metadata

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
             <title>IMPACTFUL</title>
             <meta name="description" content="Community and Sources for Tech + Social Impact" />
             <meta property="og:title" content="Impactful Community" />
-            <meta property="og:image" content="%PUBLIC_URL%/intro1.png" />
+            <meta property="og:image" content="%PUBLIC_URL%/intro1.jpg" />
           </MetaTags>
 
       <div id="ourHeaderNav">
