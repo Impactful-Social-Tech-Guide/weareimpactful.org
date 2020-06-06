2d752058cc0fe04a6fffa1cdf60e266903bd410a
iguzmanl <ivonneaguzmanl@gmail.com>
2020-06-06 12:49:54 -0700

    image metadata  fix

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
             <title>IMPACTFUL</title>
             <meta name="og:description" content="Community and Sources for Tech + Social Impact" />
             <meta property="og:title" content="Impactful Community" />
-        <meta property="og:image" content="http://weareimpactful.org/static/media/intro1.3b4ec332.svg" />
+        <meta property="og:image" content="http://weareimpactful.org/intro1.jpg" />
           </MetaTags>
 
       <div id="ourHeaderNav">
