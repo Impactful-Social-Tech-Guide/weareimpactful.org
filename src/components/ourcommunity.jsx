653f74fa99bed0bf885c2f964a395ae15fe0f0be
nsubbaian <nithmails1@gmail.com>
2020-10-15 17:04:28 -0400

    added socials and cleaned up files

--- src/components/ourcommunity.jsx
+++ src/components/ourcommunity.jsx
@@ -12,22 +12,16 @@ export default class jobOpp extends Component {
             <h1 id="page-largeheading">Slack Channel</h1>
             <p id="page-body"> Apply to join our active Slack channel where members share resources and discuss social impact and technology. </p>
             <p  id="page-body">You'll have access to our community calls and speaker events, where we discuss social impact topics and careers.</p>
+              <a class="float-left" href='https://airtable.com/shrHRVGAqiv3fVXCH' target="_blank">
+                <Button  style={{width:"240px"}} id="page-button" bsClass="flat" size="lg">Apply</Button></a>
           </Col>
 
           <Col style={{paddingTop:"20px", paddingBottom:"10px", minWidth:"350px"}}>
             <h1 id="page-largeheading">Mailing List</h1>
             <p  id="page-body"> Sign-up to receive updates about the impactful website and news about social impact opportunites. </p>
             <p  id="page-body"> Anyone can subscribe for free, and you can unsubscribe at any time. </p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <a class="float-left" href='https://airtable.com/shrHRVGAqiv3fVXCH' target="_blank">
-              <Button  style={{width:"240px"}} id="page-button" bsClass="flat" size="lg">Apply</Button></a>
-          </Col>
-          <Col>
-            <a class="float-left" href="https://airtable.com/shrz6v3BtuOsJuQbR" target="_blank">
-              <Button  style={{width:"240px"}}  id="page-button" bsClass="flat" size="lg">Subscribe</Button> </a>
+              <a class="float-left" href="https://airtable.com/shrz6v3BtuOsJuQbR" target="_blank">
+                <Button  style={{width:"240px"}}  id="page-button" bsClass="flat" size="lg">Subscribe</Button> </a>
           </Col>
         </Row>
       </Container>
