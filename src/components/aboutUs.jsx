91d904d64aed453727fb115ae4aa120447f504e2
Nithi Subbaian <nithmails1@gmail.com>
2020-10-15 22:59:38 -0400

    Update aboutUs.jsx

--- src/components/aboutUs.jsx
+++ src/components/aboutUs.jsx
@@ -38,12 +38,8 @@ export default class LandingPage extends Component {
           <Col style={{
               minWidth: "350px"
             }}>
-            <p id="aboutUs-body">
-<<<<<<< HEAD
-              Have any feedback, questions, or comments about our platform? Looking to collaborate or join our team? Please drop us a line at {" "}
-=======
-              Have any feedback, questions, or comments about our platform? Looking to collaborate or join our team? Please drop us a line at 
->>>>>>> 682e9ae086d06bef69a03aa21551fe7b81d6df06
+            <p id="aboutUs-body">Have any feedback, questions, or comments about our platform? Looking to collaborate or join our team? Please drop us a line at {" "}
+
               <a style={{
                   color: "white"
                 }} href="mailto:hello@weareimpactful.org">
