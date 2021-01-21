0b121cceb26e99116263a6807a774f494cbc52a4
nsubbaian <nithmails1@gmail.com>
2021-01-20 20:00:30 -0500

    airtable api call to console

--- src/App.js
+++ src/App.js
@@ -11,8 +11,34 @@ import ReactGA from 'react-ga';
 ReactGA.initialize('UA-167361197-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
-function App() {
-  return (
+
+  var Airtable = require('airtable');
+  var base = new Airtable({apiKey: 'keyc3AwPXakaUbISa'}).base('applLdgY5HJ2u1nLN');
+
+  base('Opportunities').select({
+      // Selecting the first 3 records in For Internal Use Only: Do Not Share:
+      maxRecords: 15,
+      view: "For Internal Use Only: Do Not Share"
+  }).eachPage(function page(records, fetchNextPage) {
+      // This function (`page`) will get called for each page of records.
+
+      records.forEach(function(record) {
+          console.log(record.get('Name'), record.get('Opportunity Type'));
+      });
+
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
+
+  }, function done(err) {
+      if (err) { console.error(err); return; }
+  });
+
+class App extends React.Component {
+
+render() {
+  return(
 
     <div className="App">
       <MetaTags>
@@ -31,6 +57,7 @@ function App() {
       </div>
     </div>
   );
+  }
 }
 
 export default App;
